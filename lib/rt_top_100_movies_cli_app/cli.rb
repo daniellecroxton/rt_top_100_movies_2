7de762d42ac2c8f61a02fbe73a1a087790f84583
daniellecroxton <danikrist@gmail.com>
2017-09-01 11:24:34 -0600

    changed when scraping details happens and added whitespace stripping

--- lib/rt_top_100_movies_cli_app/cli.rb
+++ lib/rt_top_100_movies_cli_app/cli.rb
@@ -6,7 +6,7 @@ class RtTop100MoviesCliApp::CLI
     puts ""
     puts "Welcome cinephile! Which of Rotten Tomatoes' Top 100 Movies would you like to see?"
     create_movies
-    add_movie_details
+    # add_movie_details
     start
   end
 
@@ -17,13 +17,13 @@ class RtTop100MoviesCliApp::CLI
     end
   end
 
-  def add_movie_details
-    RtTop100MoviesCliApp::Movie.all.each do | movie |
-      # binding.pry
-      details_hash = RtTop100MoviesCliApp::Scraper.scrape_movie("https://www.rottentomatoes.com#{movie.movie_url}")
-      movie.add_details(details_hash)
-    end
-  end
+  # def add_movie_details
+  #   RtTop100MoviesCliApp::Movie.all.each do | movie |
+  #     # binding.pry
+  #     details_hash = RtTop100MoviesCliApp::Scraper.scrape_movie("https://www.rottentomatoes.com#{movie.movie_url}")
+  #     movie.add_details(details_hash)
+  #   end
+  # end
 
   def start
     puts ""
@@ -37,9 +37,11 @@ class RtTop100MoviesCliApp::CLI
     input = gets.to_i
 
     @selected_movie = RtTop100MoviesCliApp::Movie.all[input-1]
+    # binding.pry
 
     if input>=1 && input<=100
-      display_movie_details(selected_movie)
+      add_movie_details(@selected_movie)
+      display_movie_details(@selected_movie)
     else
       puts "I'm not quite sure what you meant."
       start
@@ -88,21 +90,26 @@ class RtTop100MoviesCliApp::CLI
     end
   end
 
+  def add_movie_details(selected_movie)
+      details_hash = RtTop100MoviesCliApp::Scraper.scrape_movie("https://www.rottentomatoes.com#{@selected_movie.movie_url}")
+      @selected_movie.add_details(details_hash)
+  end
+
   def display_movie_details(input)
       puts ""
-      puts "********* Best of Rotten Tomatoes: #{@selected_movie[:title]} *********"
+      puts "********* Best of Rotten Tomatoes: #{@selected_movie.title} *********"
       puts ""
-      puts "Title:  #{@selected_movie[:title]}"
+      puts "Title:  #{@selected_movie.title}"
       puts ""
-      puts "Tomatometer Score: #{@selected_movie[:tomatometer_score]}"
-      puts "Audience Score: #{@selected_movie[:audience_score]}"
-      puts "Critic Consensus: #{@selected_movie[:critic_consensus]}"
+      puts "Tomatometer Score: #{@selected_movie.tomatometer_score}"
+      puts "Audience Score: #{@selected_movie.audience_score}"
+      puts "Critic Consensus: #{@selected_movie.critic_consensus}"
       puts ""
-      puts "Rated: #{@selected_movie[:rating]}"
-      puts "Genre: #{@selected_movie[:genre]}"
-      puts "Released: #{@selected_movie[:release_date]}"
-      puts "Directed by: #{@selected_movie[:director]}"
-      puts "Synopsis: #{@selected_movie[:synopsis]}"
+      puts "Rated: #{@selected_movie.rating}"
+      puts "Genre: #{@selected_movie.genre}"
+      puts "Released: #{@selected_movie.release_date}"
+      puts "Directed by: #{@selected_movie.director}"
+      puts "Synopsis: #{@selected_movie.synopsis}"
       puts ""
   end
 
--- lib/rt_top_100_movies_cli_app/scraper.rb
+++ lib/rt_top_100_movies_cli_app/scraper.rb
@@ -25,11 +25,10 @@ class RtTop100MoviesCliApp::Scraper
 
       movie_details[:tomatometer_score] = detail.css("#scorePanel .tab-content #all-critics-numbers a .meter-value").text
       movie_details[:audience_score] = detail.css(".audience-score a .superPageFontColor").first.text
-      movie_details[:critic_consensus] = detail.css(".critic_consensus.tomato-info.noSpacing.superPageFontColor").text.strip #needs regex to remove Critic Consensus:\n
-      movie_details[:synopsis] = detail.css("#movieSynopsis").text.strip
+      movie_details[:critic_consensus] = detail.css(".critic_consensus.tomato-info.noSpacing.superPageFontColor").text.strip.sub("Critic Consensus:\n", "")
       movie_details[:rating] = detail.css("div.panel-body ul.content-meta li .meta-value").first.text.strip
-      movie_details[:release_date] = detail.css("div.panel-body ul.content-meta li .meta-value")[4].text.strip
-      movie_details[:genre] = detail.css("div.panel-body ul.content-meta li .meta-value")[1].text.strip
+      movie_details[:release_date] = detail.css("div.panel-body ul.content-meta li .meta-value")[4].text.strip.gsub(/\s+/, "")
+      movie_details[:genre] = detail.css("div.panel-body ul.content-meta li .meta-value")[1].text.strip.gsub(/\s+/, "")
       movie_details[:director] = detail.css("div.panel-body ul.content-meta li .meta-value")[2].text.strip
     end
 
